02bd07bc2e85c128cbe5428b5c98de62d2c86f54
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2014-06-07 16:38:28 +0200

    Add batch launcher script

---
+++ run/batch/BatchJobs.py
@@ -0,0 +1,94 @@
+import os
+import subprocess
+import stat
+import sys
+import random
+import ROOT
+
+
+class BatchJobs:
+    def __init__(self):
+        self.name = "fa3VsfL1"
+        self.hardScatterFile = ""
+        self.minBiasFiles = []
+        self.tree = "hgcSimHitsAnalyzer/HGC"
+        self.outpuDir = ""
+        self.eventsPerJob = 100
+        self.nPileup = 200
+        self.nJobs = 0
+
+        self.cmsswDir = "/home/llr/cms/sauvan/CMSSW/EmptyArea/CMSSW_5_3_5/"
+        self.cwd = os.getcwd()
+        self.queue = "cms"
+        self.qsubArgs = []
+
+    def createConfigs(self):
+        if not os.path.exists(self.outputDir+"/jobs/"):
+            os.makedirs(self.outputDir+"/jobs/")
+        if not os.path.exists(self.outputDir+"/logs/"):
+            os.makedirs(self.outputDir+"/logs/")
+        inputFile = ROOT.TFile.Open(self.hardScatterFile)
+        tree = inputFile.Get(self.tree)
+        tree.__class__ = ROOT.TTree
+        nEvents = tree.GetEntries()
+        inputFile.Close()
+        self.nJobs = nEvents/self.eventsPerJob
+        if nEvents%self.eventsPerJob!=0: self.nJobs += 1
+        for job in range(0,self.nJobs):
+            firstEntry = job*self.eventsPerJob
+            lastEntry = min( (job+1)*self.eventsPerJob, nEvents)
+            with open(self.outputDir+"/jobs/{0}_{1}.config".format(self.name,job), 'w') as config:
+                print >>config, "HardScatterFile: {0}".format(self.hardScatterFile)
+                print >>config, "MinBiasFile: {0}".format(random.choice(self.minBiasFiles))
+                print >>config, "OutputFile: {0}/{1}_{2}.root".format(self.outputDir,self.name,job)
+                print >>config, "Tree: {0}".format(self.tree)
+                print >>config, "FirstEntry: {0}".format(firstEntry)
+                print >>config, "LastEntry: {0}".format(lastEntry)
+                print >>config, "NPileup: {0}".format(self.nPileup)
+
+
+    def createScripts(self):
+        for job in range(0,self.nJobs):
+            with open(self.outputDir+"/jobs/{0}_{1}.sub".format(self.name,job), 'w') as script:
+                print >>script, "#! /bin/sh"
+                print >>script, "export SCRAM_ARCH=slc6_amd64_gcc472"
+                print >>script, "source /opt/exp_soft/cms/cmsset_default.sh "
+                print >>script, "cd "+self.cmsswDir+"/src/"
+                print >>script, "cmsenv"
+                print >>script, "cd", self.outputDir
+                print >>script, "\necho Executing job"
+                print >>script, self.cwd+"/../../mixing.exe","jobs/{0}_{1}.config".format(self.name,job) , "&>", "logs/{0}_{1}.log".format(self.name,job)
+
+
+    def prepareCommands(self):
+        for job in range(0,self.nJobs):
+            qsubArgs = []
+            qsubArgs.append("-k")
+            qsubArgs.append("oe")
+            qsubArgs.append("-N")
+            qsubArgs.append(self.name+"_"+str(job))
+            qsubArgs.append("-q")
+            qsubArgs.append(self.queue+"@llrt3")
+            qsubArgs.append("-v")
+            qsubArgs.append("X509_USER_PROXY="+os.environ["HOME"]+"/.t3/proxy.cert")
+            qsubArgs.append("-V")
+            qsubArgs.append("{0}/jobs/{1}_{2}.sub".format(self.outputDir,self.name,job))
+            self.qsubArgs.append(qsubArgs)
+
+    def launch(self, local=False, simulate=False):
+        self.createConfigs()
+        self.createScripts()
+        self.prepareCommands()
+        for qsubArgs in self.qsubArgs:
+            if not local:
+                command = "qsub"
+                for arg in qsubArgs:
+                    command += " "+arg
+                print command
+                if not simulate:
+                    subprocess.call(["qsub"] + qsubArgs)
+            else:
+                if not simulate:
+                    os.chmod("{0}/jobs/{0}_{1}.sub".format(self.outputDir,self.name,job),stat.S_IRWXU)
+                    subprocess.call(["{0}/jobs/{0}_{1}.sub".format(self.outputDir,self.name,job)])
+
